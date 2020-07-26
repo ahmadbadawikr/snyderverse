dbcd5edff12e91c334ca74892f6ea3564acf2fb5
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-07-26 12:50:50 +0700

    adding interface

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+# . . .
+Product.create!(title: 'Batman v Superman',
+description:
+%{<p>
+<em>Fearing that the actions of Superman are left unchecked, Batman takes on the Man of Steel, while the world wrestles with what kind of a hero it really needs.
+</p>},
+image_url: 'Batman_v_Superman.jpg',
+price: 20.00)
+# . . .
